6d0bea5b3f3a754d588f10e528aba404bc14ab1e
Konstantin Shalygin <k0ste@cn.ru>
2015-06-23 12:45:34 +0600

    Add options

--- tumilter.py
+++ tumilter.py
@@ -1,18 +1,35 @@
+#!/usr/bin/python2
 
 import Milter
 import string
+import time
+import sys
+from optparse import OptionParser
 
-recipients = 2	# define maximum number of recipients int To an Cc here
-socketname = "/tmp/tumilter"
-
+def main():
+  parser = OptionParser()
+  parser.add_option('-s', '--socket', dest='socket', help='Socket to listen (e.g. inet:8890@localhost)')
+  parser.add_option('-t', '--timeout', type='int', dest='timeout', default='10', help='MTA timeout (default is 10 sec)')
+  (options, args) = parser.parse_args()
+  if not options.socket:
+    parser.print_help()
+    sys.exit(0)
+    
+  Milter.factory = TuMilter
+  print "%s TuMilter startup" % time.strftime('%Y %b %d %H:%M:%S')
+  sys.stdout.flush()
+  Milter.runmilter("tumilter",options.socket,options.timeout)
+  
 class TuMilter(Milter.Milter):
   "Milter to limit the numer of To and Cc recipients"
 
   def __init__(self):
     self.tocc = 0
+    self.id = Milter.uniqueID()
+    self.recipients = 2
 
   def header(self,name,val):
-
+    
     sv = self.getsymval('{auth_authen}')
     if sv==None or sv=="":
       return Milter.CONTINUE
@@ -22,13 +39,12 @@ class TuMilter(Milter.Milter):
       mails = string.split(val, ',')
       self.tocc=self.tocc+len(mails)
 
-    if self.tocc >= recipients:
-      self.setreply('550',None,'Sorry, your message has too many recipients')
+    if self.tocc >= self.recipients:
+      self.setreply('550', '5.5.3', 'Sorry, your message has too many recipients')
       return Milter.REJECT
    
     return Milter.CONTINUE
     
-
 if __name__ == "__main__":
-  Milter.factory = TuMilter
-  Milter.runmilter("tumilter",socketname,240)
+  main()
+  
